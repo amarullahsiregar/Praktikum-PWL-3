0c0d7d668c28bb336a0962473aac945b79971689
Rahman Amarullah - Windows <45584180+amarullahsiregar@users.noreply.github.com>
2021-05-26 12:18:34 +0700

    Revert "Revert "clear""
    This reverts commit f3689f213ef4e54c82f78526094291b27a355212.

--- src/index.js
+++ src/index.js
@@ -5,16 +5,6 @@ import Header from './myHeader'
 
 let judul = "Prak PWL 3"
 
-class Car extends React.Component {
-  constructor() {
-    super();
-    this.color = "blue"
-  }
-  render() {
-    return <h2>I am a {this.props.color} Car!</h2>;
-  }
-}
-
 ReactDOM.render(judul, document.getElementById('judul'));
 ReactDOM.render(<Header/>, document.getElementById('header'));
 ReactDOM.render(<Register/>, document.getElementById('body'));
