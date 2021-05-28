33dac845ca8e43b75d490f82f4ba09e7a3cead4b
Rahman Amarullah - Windows <45584180+amarullahsiregar@users.noreply.github.com>
2021-05-28 09:38:34 +0700

    27 mei

--- praktikum.py
+++ praktikum.py
@@ -1,28 +1,31 @@
-from time import time
-from turtle import delay
-from urllib import response
-from selenium import webdriver
 import json
-
-# options = webdriver.ChromeOptions()
-# options.add_experimental_option('excludeSwitches', ['enable-logging'])
-# driver = webdriver.Chrome(options=options)
+from lib2to3.pgen2 import driver
+from selenium import webdriver
 
 PATH = "C:\Program Files\chromedriver.exe"
-LINK = "https://prak-web-3.herokuapp.com/"
+LINK = "http://localhost:3000/"
 
 with open('prak-3.json') as json_file:
     data = json.load(json_file)
 
-for i in data:
-    browser = webdriver.Edge(PATH)
-    browser.get(LINK)
 
+for i in data:
+    browser = webdriver.Chrome(PATH)
+    browser.get(LINK)
+    print(browser.title+"\n\n")
     browser.find_element_by_name('name').send_keys(i['data']['name'])
     browser.find_element_by_name('surname').send_keys(i['data']['surname'])
     browser.find_element_by_name('username').send_keys(i['data']['username'])
     browser.find_element_by_name('password').send_keys(i['data']['password'])
     browser.find_element_by_name('password2').send_keys(i['data']['password2'])
-    klik = browser.find_element_by_id('register_submit')
+    submit = browser.find_element_by_id('register_submit')
 
-    klik.click()
+    submit.click()
+    try:
+        alert = browser.switch_to_alert()
+        pesan = alert.text
+        alert.accept()
+        print("\nIsi pesan : "+pesan+"\n")
+    except:
+        print("\nGagal !\n")
+browser.close()
--- src/Register.js
+++ src/Register.js
@@ -3,20 +3,58 @@ import './register.css';
 import logo from './register_logo.png';
 
 class Register extends React.Component{
-    jikaKosong(){
-      var namefield = document.getElementById('name')
-      var usernamefield = document.getElementById('username')
-      var passwordfield = document.getElementById('password')
-        if (namefield.value!==""
-            &&usernamefield.value!==""
-            &&passwordfield.value!=="") {
-          alert("Berhasil !")
-        }else{
-          namefield.placeholder="Nama tidak boleh kosong *"
-          usernamefield.placeholder="Username tidak boleh kosong *"
-          alert("Kolom bertanda * Tidak Boleh Kosong")
-        }
+  constructor () {
+    super();
+    this.state = {
+      input: {
+        'name': '',
+        'surname': '',
+        'username': '',
+        'password': '',
+        'password2': ''
+      },
+      errors:{}
     }
+  }
+
+  submit(){
+    let input = this.state.input;
+    input.name=document.getElementById('name').value
+    input.surname=document.getElementById('surname').value
+    input.username=document.getElementById('username').value
+    input.password=document.getElementById('password').value
+    input.password2=document.getElementById('password2').value
+    
+
+    this.cekIsi(this)
+  }
+
+
+  cekIsi(){
+    let input = this.state.input;
+    this.errors=""
+
+    if(!input['name']){
+      this.errors=" Nama,"
+    }
+    if(!input['username']){
+      this.errors=this.errors+" Username,"
+    }
+    if(!input['password']){
+      this.errors=this.errors+" Password"
+    }
+    if(input['password']!==input['password2']){
+      this.errors=this.errors+" dan Pastikan re-password sesuai"
+    }
+    if(this.errors!==""){
+      alert("Lengkapi Kolom"+this.errors)
+      this.errors=""
+    }else{
+      alert("Berhasil")
+    }
+    }
+
+
     render() {
       return(
       <div className="App">
@@ -30,11 +68,11 @@ class Register extends React.Component{
               <img src={logo} className="App-logo" alt="logo" />
               <h1><b>Sign up</b></h1>
               <input type="text" name="name" id="name" placeholder="Nama Depan*"/>
-              <input type="text" name="surname" placeholder="Nama Keluarga" />
+              <input type="text" name="surname" id="surname" placeholder="Nama Keluarga" />
               <input type="text" name="username" id="username" placeholder="Username *"/>
-              <input type="password" name="password" placeholder="Password *"/>
-              <input type="password" name="password2" placeholder="Retype password *"/>
-              <input type="submit" onClick={this.jikaKosong.bind(this)} id="register_submit" value="Register" />
+              <input type="password" name="password" id="password" placeholder="Password *"/>
+              <input type="password" name="password2" id="password2" placeholder="Retype password *"/>
+              <input type="submit" onClick={this.submit.bind(this)} id="register_submit" value="Register" />
           </div>
       </div>
       )
